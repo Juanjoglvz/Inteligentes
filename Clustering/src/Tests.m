8cf474358bcb5a62e9f2de9bf5e03b1cba19b48f
JUAN JOSÉ CORROTO MARTÍN <JuanJose.Corroto@alu.uclm.es>
2020-03-27 13:12:52 +0100

    Mas hito 2

--- Clustering/src/Tests.m
+++ Clustering/src/Tests.m
@@ -1,12 +1,36 @@
-raw = readtable("groups.csv");
+% Read data
+raw = readtable("groups_a_priori.csv");
+
+
+% Extract groups column
 group = table2array(raw(:,"Group"));
 
+
+% Perform ANOVA test for each column
 variables = ["Delicassen", "Detergents_Paper", "Fresh", "Frozen", "Grocery", "Milk"];
+
 p_values = zeros(length(variables));
 for i = 1:length(variables)
    disp(variables(i));
-   data = table2array(raw(:,variables(i)));
-   p_values(i) = anova1(data, group);
+   data = raw(:,variables(i));
+   p_values(i) = kruskalwallis(data, group);
 end
 
 disp(p_values);
+
+% Transform table into matlab matrix
+raw_data = table2array(raw);
+
+% Calculate centroids for each group using median
+centroids = zeros(6, length(variables));
+for i = 1:6
+    rows = raw_data(:, 9) == i-1;
+    for j = 1:length(variables)
+       data = table2array(raw(rows, variables(j)));
+       centroids(i, j) = median(data);
+    end
+end
+
+disp(centroids);
+
+
